fd4e7045d4975b989bd5e2c7656e88257b553392
RyLaird <lairdry@gmail.com>
2022-04-20 16:51:43 -0500

    created map

--- src/App.vue
+++ src/App.vue
@@ -2,59 +2,113 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="#CC0100"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <v-container>
+          <v-card elevation-10>
+            <l-map ref="myMap"
+              :zoom="zoom"
+              :center="center"
+              style="height: 720px"
+            >
+              <!-- sourcing -->
+              <l-tile-layer
+                :url="url"
+                :attribution="attribution"
+              />
+              <l-control-scale position="topright" :imperial="true" :metric="false"></l-control-scale>
+              <!-- pop density geojson -->
+              <l-geo-json
+                :geojson="geojson_popden"
+                :options="options"
+                :options-style="styleFunction_popden"
+              />
+            </l-map>
+          </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import Map from './components/Map';
+import { LMap, LTileLayer, LGeoJson, LControlScale} from "vue2-leaflet";
+import 'leaflet/dist/leaflet.css'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    LMap,
+    LTileLayer,
+    LGeoJson,
+    LControlScale
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      map: true,
+      enableTooltip: true,
+      zoom: 6,
+      center: [47.60665052929262, -122.33503601679615],
+      geojson_popden: null,
+      url: 'https://api.mapbox.com/styles/v1/rlaird2/ckwo8jew23mns14lrzvnnxh63/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmxhaXJkMiIsImEiOiJja2JmN2x6aWIwc3VmMzVvNDl5Mzk1ejNuIn0.rrNaMaCy39_ntp7qPvp0dQ',
+       attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions>CARTO</a>',
+    }
+  },
+  computed: {
+    options() {
+      return {
+        onEachFeature: this.onEachFeatureFunction1
+      }
+    },
+    styleFunction_popden() {
+      return () => {
+        return {
+          weight: 1,
+          color: "#ECEFF1",
+          opacity: 1,
+          fillColor: "#7E57C2",
+          fillOpacity: 1
+        };
+      };
+    },
+    onEachFeatureFunction1() {
+      if (!this.enableTooltip) {
+        return () => {};
+      }
+      return (feature, layer) => {
+        layer.bindPopup(
+          "<div>Name: " +
+            feature.properties.HA_NAME +
+            "</div><div>Estimated Horses: " +
+            feature.properties.EST_HORSE_POP +
+            "</div><div> Estimated Burros: " +
+            feature.properties.EST_BURRO_POP +
+          "</div>",
+          { permanent: false, sticky: true }
+        );
+      };
+    },
+  },
+  async created() {
+    this.loading = true;
+    const response_popden = await fetch("https://raw.githack.com/RyLaird/vue-wild-horse-and-burro-program/master/src/assets/whb_ha_pop_poly.geojson")
+    const data_popden = await response_popden.json();
+    this.geojson_popden = data_popden;
+    this.loading = false;
+  },
+  methods: {
+    recenterMap() {
+      this.$refs.myMap.mapObject.flyTo([39, -105], 6)
+    },
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import 'leaflet/dist/leaflet.css';
 
 Vue.config.productionTip = false
 
