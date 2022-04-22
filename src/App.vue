c4387e0ce3d9a9e6b106301b4063d6e920b4cfea
RyLaird <lairdry@gmail.com>
2022-04-22 16:42:44 -0500

    added circlemarker proportional symbols

--- src/App.vue
+++ src/App.vue
@@ -30,9 +30,18 @@
                 :options-style="styleFunction_popden"
                 @click="zoomToFeature()"
               />
-              <l-geo-json
-              :geojson="geojson_targets"
-              :options="markerOptions"
+              <!-- <l-geo-json
+                :geojson="geojson_targets"
+                :options="markerOptions"
+                :options-style="styleFunction_marker"
+              /> -->
+              <l-circle-marker v-for="circle in circles"
+                :key="circle.id"
+                :lat-lng="circle.center"
+                :radius="circle.radius"
+                :color="circle.color"
+                :fillColor="circle.fillColor"
+                :fillOpacity="circle.fillOpacity"
               />
             </l-map>
           </v-card>
@@ -59,22 +68,24 @@
 
 <script>
 // import Map from './components/Map';
-import { LMap, LTileLayer, LGeoJson, LControlScale} from "vue2-leaflet";
+import { LMap, LTileLayer, LGeoJson, LControlScale, LCircleMarker} from "vue2-leaflet";
 import 'leaflet/dist/leaflet.css'
-import {Icon} from 'leaflet';
+// import {Icon} from 'leaflet';
 import Chart from '/Users/rlaird/Documents/targets-ranked-seattle-tacoma-bellevue/src/components/Chart.vue'
 
 
+// const iconSize = [60,60]
+// delete Icon?.Default.prototype._getIconUrl;
+// Icon.Default.mergeOptions({
+//   iconRetinaUrl: require('/src/assets/target_logo.png'),
+//   iconUrl: require('/src/assets/target_logo.png'),
+//   // iconAnchor: [0,0],
+//   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+//   shadowSize: [30,30],
+//   iconSize: iconSize
+// });
 
-delete Icon.Default.prototype._getIconUrl;
-Icon.Default.mergeOptions({
-  iconRetinaUrl: require('/src/assets/target_logo.png'),
-  iconUrl: require('/src/assets/target_logo.png'),
-  // iconAnchor: [0,0],
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-  shadowSize: [30,30],
-  iconSize: [50,50]
-});
+
 
 export default {
   name: 'App',
@@ -85,20 +96,171 @@ export default {
     Chart,
     LGeoJson,
     LControlScale,
+    LCircleMarker
   },
 
 
   data() {
+
     return {
       map: true,
       enableTooltip: true,
       zoom: 10,
       center: [47.60665052929262, -122.33503601679615],
       geojson_popden: null,
-      geojson_targets: null,
+      // geojson_targets: null,
       url: 'https://api.mapbox.com/styles/v1/rlaird2/cl29hg7ah000914my0rv48xwe/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmxhaXJkMiIsImEiOiJja2JmN2x6aWIwc3VmMzVvNDl5Mzk1ejNuIn0.rrNaMaCy39_ntp7qPvp0dQ',
       attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions>CARTO</a>',
+
+      // circle1: {
+      //   center: [47.8325549090366, -122.265984791228],
+      //   radius: 30,
+      //   color: '#CC0100',
+      //   fillColor: '#CC0100',
+      //   fillOpacity: .6
+      // },
+      circles: [ { id: '1',
+          center: [47.8325549090366, -122.265984791228],
+          radius: 2036124/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '2',
+          center: [47.2377519755425, -122.480298527615],
+          radius: 1994079/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '3',
+          center: [47.7590355176595, -122.154068022324],
+          radius: 1755491/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '4',
+          center: [47.1599769333971, -122.295223147819],
+          radius: 1548847/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '5',
+          center: [47.5417638713567, -122.050644059283],
+          radius: 1529826/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '6',
+          center: [47.1128888508873, -122.291666210924],
+          radius: 1527335/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '7',
+          center: [47.4962571449308, -122.199851723218],
+          radius: 1485331/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '8',
+          center: [47.3130694995818, -122.305231085636],
+          radius: 1469152/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '9',
+          center: [47.4551854083819, -122.258933363672],
+          radius: 1461693/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '10',
+          center: [47.6725464028309, -122.103446825318],
+          radius: 1410518/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '11',
+          center: [47.1631128985045, -122.511436143694],
+          radius: 1410272/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '12',
+          center: [47.9104543526083, -122.227315450308],
+          radius: 1370193/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '13',
+          center: [47.3660073070965, -122.203416396167],
+          radius: 1305197/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '14',
+          center: [48.1489489002533, -122.19274811851],
+          radius: 1285735/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '15',
+          center: [47.3613164771609, -122.607974698474],
+          radius: 1278391/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '16',
+          center: [47.9991060530233, -122.101281006088],
+          radius: 1252088/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '17',
+          center: [47.5737553851301, -122.173513510975],
+          radius: 1185260/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '18',
+          center: [47.1725553918957, -122.172303032396],
+          radius: 1136295/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '19',
+          center: [47.5222520295411, -122.368754872501],
+          radius: 1050824/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        { id: '20',
+          center: [47.6086499037094, -122.338778749914],
+          radius: 997170/45000,
+          color: '#CC0100',
+          fillColor: '#CC0100',
+          fillOpacity: .6
+        },
+        ]
     }
   },
   computed: {
@@ -107,11 +269,11 @@ export default {
         onEachFeature: this.onEachFeatureFunction
       } 
     },
-    markerOptions() {
-      return {
-        onEachFeature: this.onEachMarkerFunction
-      }
-    },
+    // markerOptions() {
+    //   return {
+    //     onEachFeature: this.onEachMarkerFunction
+    //   }
+    // },
     styleFunction_popden() {
       return (feature) => {
         return {
@@ -123,8 +285,14 @@ export default {
           fillOpacity: .4
         };
       };
-
     },
+    // styleFunction_marker() {
+    //   return (feature) => {
+    //     return {
+    //     iconSize: this.getSize(feature.properties.Rank)
+    //   };
+    // }
+    // },
     onEachFeatureFunction() {
       if (!this.enableTooltip) {
         return () => {};
@@ -152,24 +320,24 @@ export default {
         });
       };
     },
-    onEachMarkerFunction() {
-      return (feature, layer) => {
-        var props = feature.properties
-        layer.bindPopup(
-          "<div>Visitors: " +
-          Math.round(props["Total Visits"])+
-          "</div>",
-          { permanent: false, sticky: false}
+    // onEachMarkerFunction() {
+    //   return (feature, layer) => {
+    //     var props = feature.properties
+    //     layer.bindPopup(
+    //       "<div>Visitors: " +
+    //       Math.round(props["Total Visits"])+
+    //       "</div>",
+    //       { permanent: false, sticky: false}
 
-        ),
-        layer.bindTooltip(
-          "<div> " +
-          props["Rank"] +
-          "</div>",
-          {permanent: true, sticky: false, className: "label"}
-        )
-      }
-    }
+    //     ),
+    //     layer.bindTooltip(
+    //       "<div> " +
+    //       props["Rank"] +
+    //       "</div>",
+    //       {permanent: true, sticky: false, className: "label"}
+    //     )
+    //   }
+    // },
   },
   async created() {
     this.loading = true;
@@ -178,9 +346,9 @@ export default {
     const data_popden = await response_popden.json();
     this.geojson_popden = data_popden;
 
-    const response_targets = await fetch("https://raw.githubusercontent.com/RyLaird/targets-ranked-seattle-tacoma-bellevue/master/src/data/target_locations.geojson")
-    const data_targets = await response_targets.json();
-    this.geojson_targets = data_targets;
+    // const response_targets = await fetch("https://raw.githubusercontent.com/RyLaird/targets-ranked-seattle-tacoma-bellevue/master/src/data/target_locations.geojson")
+    // const data_targets = await response_targets.json();
+    // this.geojson_targets = data_targets;
     
     this.loading = false;
   },
@@ -199,10 +367,10 @@ export default {
       d > 1500 ? '#bfd3e6':
       '#edf8fb';
     },
-    getSize(d) {
-      return d > 2000000 ? '[100,100]' :
-      '[50,50]';
-    }
+    // getSize(d) {
+    //   return d > 10 ? [200,200]:
+    //   [30,30];
+    // }
   }
 };
 </script>
