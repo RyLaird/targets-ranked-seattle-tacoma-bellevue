977c83d0ed4b67de4cfde690e16803d15d813605
RyLaird <lairdry@gmail.com>
2022-04-22 11:12:06 -0500

    created echarts scatter plot

--- src/App.vue
+++ src/App.vue
@@ -31,12 +31,28 @@
                 @click="zoomToFeature()"
               />
               <l-geo-json
-              :geojson="target_locations"
+              :geojson="geojson_targets"
               :options="markerOptions"
               />
             </l-map>
           </v-card>
       </v-container>
+      <div>
+        <v-card class="mt-5 mb-8">
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col>
+                <h1 class='text-center mb-5'>
+                  Annual Target Foot Traffic (2021)
+                  </h1>
+                  <chart></chart>
+                  </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+        </v-card>
+        </div>
     </v-main>
   </v-app>
 </template>
@@ -45,10 +61,20 @@
 // import Map from './components/Map';
 import { LMap, LTileLayer, LGeoJson, LControlScale} from "vue2-leaflet";
 import 'leaflet/dist/leaflet.css'
+import {Icon} from 'leaflet';
+import Chart from '/Users/rlaird/Documents/targets-ranked-seattle-tacoma-bellevue/src/components/Chart.vue'
 
 
 
-
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('/src/assets/target_logo.png'),
+  iconUrl: require('/src/assets/target_logo.png'),
+  // iconAnchor: [0,0],
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  shadowSize: [30,30],
+  iconSize: [50,50]
+});
 
 export default {
   name: 'App',
@@ -56,8 +82,9 @@ export default {
   components: {
     LMap,
     LTileLayer,
+    Chart,
     LGeoJson,
-    LControlScale
+    LControlScale,
   },
 
 
@@ -70,7 +97,7 @@ export default {
       geojson_popden: null,
       geojson_targets: null,
       url: 'https://api.mapbox.com/styles/v1/rlaird2/cl29hg7ah000914my0rv48xwe/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmxhaXJkMiIsImEiOiJja2JmN2x6aWIwc3VmMzVvNDl5Mzk1ejNuIn0.rrNaMaCy39_ntp7qPvp0dQ',
-       attribution:
+      attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions>CARTO</a>',
     }
   },
@@ -88,41 +115,61 @@ export default {
     styleFunction_popden() {
       return (feature) => {
         return {
-          weight: .5,
+          weight: 1,
           color: "white",
-          opacity: .7,
+          opacity: .5,
           dashArray: '',
           fillColor: this.getColor(feature.properties.popDensity),
-          fillOpacity: .6
+          fillOpacity: .4
         };
       };
+
     },
     onEachFeatureFunction() {
       if (!this.enableTooltip) {
         return () => {};
       }
       return (feature, layer) => {
-        layer.bindTooltip(
+        layer.bindPopup(
           "<div>Population Density: " +
-            feature.properties.popDensity +
+            Math.round(feature.properties.popDensity) +
           "</div>",
           { permanent: false, sticky: true }
         ),
         layer.on('mouseover', function (e){
           e.target.setStyle({
-            weight:4,
-            dashArray: '',
+            weight:6,
+            color: 'white',
+            opacity: .5
           })
         });
         layer.on('mouseout', function (e){
           e.target.setStyle({
-            weight: .5,
-            dashArray: '6',
-            opacity: .7
+            weight: 1,
+            color: "white",
+            opacity: .5
           })
         });
       };
     },
+    onEachMarkerFunction() {
+      return (feature, layer) => {
+        var props = feature.properties
+        layer.bindPopup(
+          "<div>Visitors: " +
+          Math.round(props["Total Visits"])+
+          "</div>",
+          { permanent: false, sticky: false}
+
+        ),
+        layer.bindTooltip(
+          "<div> " +
+          props["Rank"] +
+          "</div>",
+          {permanent: true, sticky: false, className: "label"}
+        )
+      }
+    }
   },
   async created() {
     this.loading = true;
@@ -131,7 +178,7 @@ export default {
     const data_popden = await response_popden.json();
     this.geojson_popden = data_popden;
 
-    const response_targets = await fetch("https://raw.githack.com/RyLaird/targets-ranked-seattle-tacoma-bellevue/master/src/data/target_locations.geojson")
+    const response_targets = await fetch("https://raw.githubusercontent.com/RyLaird/targets-ranked-seattle-tacoma-bellevue/master/src/data/target_locations.geojson")
     const data_targets = await response_targets.json();
     this.geojson_targets = data_targets;
     
@@ -152,6 +199,18 @@ export default {
       d > 1500 ? '#bfd3e6':
       '#edf8fb';
     },
+    getSize(d) {
+      return d > 2000000 ? '[100,100]' :
+      '[50,50]';
+    }
   }
 };
 </script>
+
+<style scoped>
+.leaflet-tooltip {
+  background-color: transparent;
+  border: transparent;
+  box-shadow: none;
+  }
+</style>
---
+++ src/components/Chart.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="line-wrapper">
+    <v-chart :options="line" autoresize />
+  </div>
+</template>
+
+<script>
+import ECharts from "vue-echarts";
+import "echarts/lib/chart/scatter";
+import "echarts/lib/chart/effectScatter"
+import 'echarts/lib/component/legend'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/title'
+import 'echarts/lib/component/grid'
+export default {
+  components: {
+      "v-chart": ECharts,
+  },
+    data: () => ({
+        line: {
+            xAxis: {
+                name: 'Total Visitors',
+                scale: true,
+                splitLine: {
+                    lineStyle: {
+                        type: 'dashed'
+                    }
+                }
+            },
+            yAxis: {
+                name: 'Population Density',
+                scale: true,
+                    splitLine: {
+                    lineStyle: {
+                        type: 'dashed'
+                    }
+                }
+            },
+            width: '80%',
+            height: '80%',
+            series: [
+                { type: 'effectScatter',
+                name: 'Target Store Outlier',
+                symbolSize: 15,
+                color: "#B71C1C",
+                data: [
+                    [997170, 25986],
+                    [1050824, 7722],
+                ]
+                },
+                {   
+                    symbolSize: 20,
+                    data: [
+                            [1136295, 1351],
+                            [1185260, 3536],
+                            [1252088, 1151],
+                            [1278391, 897],
+                            [1285735, 625],
+                            [1305197, 5425],
+                            [1370193, 3597],
+                            [1410272, 3413],
+                            [1410518, 3492],
+                            [1461693, 3343],
+                            [1469152, 4259],
+                            [1485331, [(2101 + 4745)/2]],
+                            [1527335, [(2624 + 3288)/2]],
+                            [1529826, [(555 + 3267)/2]],
+                            [1548847, [(2800 + 2624)/2]],
+                            [1755491, [(1321+4125)/2]],
+                            [1994079, [(5720+3646)/2]],
+                            [2036124, [(5293+4208)/2]]
+                    ],
+                    type: 'scatter',
+                    name: 'Target Store',
+                    color: '#CC0100'
+                }
+            ],
+            tooltip: {
+                trigger: 'axis',    
+            },
+            legend: {
+            data: ['Target Store'],
+            left: 'center',
+            markPoint: {
+        data: [
+          { type: 'max', name: 'Max' },
+          { type: 'min', name: 'Min' }
+        ]
+      },
+  },
+        }
+    })
+}
+</script>
+
+<style scoped>
+.echarts {
+    width: 100%;
+    height: 400px;
+}
+</style>
